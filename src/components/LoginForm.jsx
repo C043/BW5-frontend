c2f942ac35fdd255c93d283825279a8821c08bd9
Mario Fragnito <mariofragnitoph@gmail.com>
2024-09-27 15:22:38 +0200

    Adds login page

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const location = useLocation();
   const navigation = useNavigate();
-  const params = useParams();
 
   const handleSubmit = async event => {
     event.preventDefault();
@@ -24,7 +22,8 @@ const LoginForm = () => {
         }),
       });
       if (resp.ok) {
-        alert("successo");
+        const data = await resp.json();
+        navigation("/clienti/" + data.token);
       } else throw new Error();
     } catch (error) {
       console.log(error);
@@ -32,26 +31,38 @@ const LoginForm = () => {
   };
 
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" value={email} onChange={ev => setEmail(ev.target.value)} />
-        <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
-      </Form.Group>
+    <>
+      <h1 className="h3 mt-3">Login</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={ev => setEmail(ev.target.value)}
+          />
+          <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={ev => setPassword(ev.target.value)}
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit" onClick={handleSubmit}>
-        Submit
-      </Button>
-    </Form>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={ev => setPassword(ev.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit" onClick={handleSubmit}>
+          Submit
+        </Button>
+      </Form>
+      <p className="mt-3 mb-0">Non hai un account?</p>
+      <Link className="mt-1" to="/register">
+        Registrati
+      </Link>
+    </>
   );
 };
 
