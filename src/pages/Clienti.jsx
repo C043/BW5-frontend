65285b13d1a91072817cb704b9a109408f571b62
Mario Fragnito <mariofragnitoph@gmail.com>
2024-09-27 14:47:28 +0200

    Adds pagina clienti

--- src/pages/Clienti.jsx
+++ src/pages/Clienti.jsx
@@ -1,16 +1,19 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ClienteCard from "../components/ClienteCard";
+import { Button } from "react-bootstrap";
 
 const Clienti = () => {
   const [hasError, setError] = useState(false);
+  const [page, setPage] = useState(0);
+  const [last, setLast] = useState(false);
   const [clienti, setClienti] = useState([]);
 
   const { token } = useParams();
 
   const fetchClienti = async () => {
     try {
-      const resp = await fetch("http://localhost:3001/clienti", {
+      const resp = await fetch("http://localhost:3001/clienti" + "?page=" + page, {
         headers: {
           Authorization: "Bearer " + token,
         },
@@ -18,6 +21,8 @@ const Clienti = () => {
       if (resp.ok) {
         const data = await resp.json();
         setClienti(data.content);
+        setLast(data.last);
+        setPage(data.number);
       } else throw new Error();
     } catch (error) {
       console.log(error);
@@ -25,10 +30,11 @@ const Clienti = () => {
   };
   useEffect(() => {
     fetchClienti();
-  }, []);
+  }, [page]);
+
   return (
     <>
-      <h1>Clienti</h1>
+      <h1 className="mt-5">Clienti</h1>
       {clienti.map(cliente => (
         <ClienteCard
           key={cliente.id}
@@ -38,6 +44,20 @@ const Clienti = () => {
           fatturatoAnnuale={cliente.fatturatoAnnuale}
         />
       ))}
+      {page === 0 ? (
+        ""
+      ) : (
+        <Button className="mb-5 me-3" onClick={() => setPage(page - 1)}>
+          Indietro
+        </Button>
+      )}
+      {last ? (
+        ""
+      ) : (
+        <Button className="mb-5" onClick={() => setPage(page + 1)}>
+          Avanti
+        </Button>
+      )}
     </>
   );
 };
