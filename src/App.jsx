52c428fe9f7745a47e5799b7ec1bf7b44a411546
Mario Fragnito <mariofragnitoph@gmail.com>
2024-09-27 12:43:22 +0200

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import { Container } from "react-bootstrap";
 
-function App() {}
+function App() {
+  return (
+    <Container>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </Container>
+  );
+}
 
 export default App;
---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+
+const LoginForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" value={email} onChange={ev => setEmail(ev.target.value)} />
+        <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={ev => setPassword(ev.target.value)}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
+
+export default LoginForm;
